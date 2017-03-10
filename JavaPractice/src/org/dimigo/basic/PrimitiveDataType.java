571fb42ef8cd4f9e688fa6b11c3460bbb5a2551e
PythonTeacher <hihamey@gmail.com>
2017-03-10 14:20:53 +0900

    실습과제1. 아이유 프로필 출력하기

---
+++ JavaPractice/src/org/dimigo/basic/PrimitiveDataType.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package org.dimigo.basic;
+
+/**
+ * <pre>
+ *  org.dimigo.basic
+ *    |_ PrimitiveDataType
+ * 
+ * 1. 개요 : 아이유 프로필
+ * 2. 작성일 : 2017. 3. 9.
+ * </pre>
+ *
+ * @author : teacher
+ * @version : 1.0
+ */
+public class PrimitiveDataType {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+
+	}
+
+}
